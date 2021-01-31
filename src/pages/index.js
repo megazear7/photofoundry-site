63476dc78a2e559f01c789005291e079c3676d76
Alex Lockhart <alexlockahrt7@gmail.com>
2021-01-31 07:00:40 -0500

    Updated icons and colors

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,14 @@ import { html } from 'orison';
 export default context => html`
   <section>
     <div>
-      <a href="https://github.com/megazear7/photofoundry">Photofoundry on Github</a>
+      <img class="center img-128" src="/icons/icon-128x128.png"></img>
     </div>
 
     ${context.mdFile('./src/partials/getting-started.md')}
+
+    <div class="center">
+      <a href="https://github.com/megazear7/photofoundry">Photofoundry on Github</a>
+    </div>
+    <br>
   </section>
 `;
--- src/partials/footer.js
+++ src/partials/footer.js
@@ -2,6 +2,11 @@ import { html } from 'orison';
 
 export default () => html`
   <footer>
+    <div>
+      <img class="center img-128" src="/icons/icon-128x128.png"></img>
+    </div>
     <p>Built with the OrisonJS Starter Kit</p>
+    <p><a href="https://github.com/megazear7/photofoundry">Photofoundry on Github</a></p>
+    <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
   </footer>
 `;

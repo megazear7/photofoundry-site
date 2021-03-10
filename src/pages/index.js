0b534d1185b77532e7d801f1bdd06a8edabcc978
Alex Lockhart <alexlockahrt7@gmail.com>
2021-03-10 05:08:36 -0500

    Added support links

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,7 @@ export default context => html`
     <div>
       <img class="center img-128" src="/icons/icon-128x128.png"></img>
     </div>
-
     ${context.mdFile('./src/partials/getting-started.md')}
-
-    <div class="center">
-      <a href="https://github.com/megazear7/photofoundry">Photofoundry on Github</a>
-    </div>
     <br>
   </section>
 `;
--- src/partials/footer.js
+++ src/partials/footer.js
@@ -5,8 +5,14 @@ export default () => html`
     <div>
       <img class="center img-128" src="/icons/icon-128x128.png"></img>
     </div>
-    <p>Built with the OrisonJS Starter Kit</p>
-    <p><a href="https://github.com/megazear7/photofoundry">Photofoundry on Github</a></p>
+    <ul>
+      <li><a href="https://github.com/megazear7/photofoundry">Github</a></li>
+      <li><a href="https://www.npmjs.com/package/photofoundry">NPM</a></li>
+      <li><a href="https://www.alexlockhart.me">Alex Lockhart</a></li>
+      <li><a href="https://www.buymeacoffee.com/alexlockhart">Support</a></li>
+      <li><a href="https://www.patreon.com/alexlockhart">Patreon</a></li>
+    </ul>
+    <p>Built with <a href="https://orison.alexlockhart.me/">OrisonJS</a></p>
     <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
   </footer>
 `;
